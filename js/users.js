56caff8453005694282f0fdf47eceef41aa2c037
Stoffus-pt <export@stoffus.pt>
2026-02-06 17:19:39 +0000

    Add files via upload

---
+++ js/users.js
@@ -0,0 +1,9 @@
+/**
+ * BASE DE DADOS DE UTILIZADORES
+ * Adicione aqui os clientes profissionais.
+ * Formato: { user: "nome_login", pass: "senha123", name: "Nome Exibido" }
+ */
+const DB_USERS_EXTERNAL = [
+    { user: "elcorteingles", pass: "1234", name: "El Corte Inglés" },
+    { user: "showroom", pass: "stoffus1974", name: "Divinus Confort" },
+];
