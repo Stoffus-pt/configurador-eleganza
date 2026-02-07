a5038d219173e8c7f4709a567dcf31fc6961b21e
Stoffus-pt <export@stoffus.pt>
2026-02-07 17:57:40 +0000

    Add NIF to user entries in DB_USERS_EXTERNAL

--- js/users.js
+++ js/users.js
@@ -4,7 +4,8 @@
  * Formato: { user: "nome_login", pass: "senha123", name: "Nome Exibido" }
  */
 const DB_USERS_EXTERNAL = [
-    { user: "elcorteingles", pass: "1234", name: "El Corte Inglés" },
+    { user: "elcorteingles", pass: "1234", name: "El Corte Inglés", nif: "501234567"},
     { user: "showroom", pass: "stoffus1974", name: "Divinus Confort", nif: "506942317"},
 
 ];
+
