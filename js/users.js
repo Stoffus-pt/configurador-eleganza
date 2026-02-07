7526a10fbef87e166e038305482d52603cc6c674
Stoffus-pt <export@stoffus.pt>
2026-02-07 18:01:10 +0000

    Format user database for better readability

--- js/users.js
+++ js/users.js
@@ -1,11 +1,19 @@
 /**
  * BASE DE DADOS DE UTILIZADORES
  * Adicione aqui os clientes profissionais.
- * Formato: { user: "nome_login", pass: "senha123", name: "Nome Exibido" }
+ * Formato: { user: "nome_login", pass: "senha123", name: "Nome Exibido", nif: "123456789" }
  */
 const DB_USERS_EXTERNAL = [
-    { user: "elcorteingles", pass: "1234", name: "El Corte Inglés", nif: "501234567"},
-    { user: "showroom", pass: "stoffus1974", name: "Divinus Confort", nif: "506942317"},
-
+    { 
+        user: "elcorteingles", 
+        pass: "1234", 
+        name: "El Corte Inglés", 
+        nif: "501234567"
+    },
+    { 
+        user: "showroom", 
+        pass: "stoffus1974", 
+        name: "Divinus Confort", 
+        nif: "506942317"
+    }
 ];
-
